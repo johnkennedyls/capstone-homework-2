7b156fe64a1f2cafdea2c1309bcc0e592873201b
John Kennedy Landazuri Sandoval <jkls1998@gmail.com>
2020-11-05 21:46:53 -0500

    final

--- src/model/ManageScore.java
+++ src/model/ManageScore.java
@@ -1,5 +1,46 @@
 package model;
 
 public class ManageScore {
+          
+	private ScoreGame root;
+	
+	public ManageScore() {
+		root = null;
+	}
+	
+	
+	public void addScoreGame() {
+		ScoreGame newScore = new ScoreGame();
+		if(root==null) {
+			root = newScore;
+		}else {
+			addScoreGame(root, newScore);
+		}
+		
+		
+		
+	}
 
+
+	private void addScoreGame(ScoreGame currentScore, ScoreGame newScore) {
+		if(currentScore.getScore() > newScore.getScore()) {
+			if(currentScore.getLeft()==null) {
+				currentScore.setLeft(newScore);
+				newScore.setParent(currentScore);
+			}else {
+				addScoreGame(currentScore.getLeft(), newScore);
+			}
+		}else {
+			if(currentScore.getRight()==null) {
+				currentScore.setRight(newScore);
+				newScore.setParent(currentScore);
+			}else {
+				addScoreGame(currentScore.getRight(), newScore);
+			}
+		}
+		
+	}
+	
+	
+	
 }
